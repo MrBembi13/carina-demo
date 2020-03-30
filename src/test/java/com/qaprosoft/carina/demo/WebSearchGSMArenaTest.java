e7f03d2a176cfeced67412d958142a3cbf36cd58
TA_1 <ta_1@Nastias-Mac-mini.local>
2020-03-30 11:52:35 +0300

    added xls data provider

--- src/test/java/com/qaprosoft/carina/demo/WebSearchGSMArenaTest.java
+++ src/test/java/com/qaprosoft/carina/demo/WebSearchGSMArenaTest.java
@@ -1,12 +1,15 @@
 package com.qaprosoft.carina.demo;
 
 import com.qaprosoft.carina.core.foundation.AbstractTest;
+import com.qaprosoft.carina.core.foundation.dataprovider.annotations.XlsDataSourceParameters;
 import com.qaprosoft.carina.core.foundation.utils.ownership.MethodOwner;
 import com.qaprosoft.carina.demo.gui.components.DropDownSearchForm;
 import com.qaprosoft.carina.demo.gui.pages.HomePage;
 import com.qaprosoft.carina.demo.gui.pages.DevicePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
 
 /**
  * Test search system on website https://www.gsmarena.com
@@ -15,22 +18,21 @@ import org.testng.annotations.Test;
  */
 
 public class WebSearchGSMArenaTest extends AbstractTest {
-    private static final String SEARCH_TEXT = "xiaomi";
-
-    @Test(description = "Test search field in header for searching device by text")
+    @Test(description = "Test search field in header for searching device by text", dataProvider = "DataProvider")
     @MethodOwner(owner = "Vasyl Rudyk")
-    public void testSearchFieldInHeader() {
+    @XlsDataSourceParameters(path = "xls/search.xlsx", sheet = "Search text", dsUid = "TUID")
+    public void testSearchFieldInHeader(HashMap<String, String> args) {
         HomePage homePage = new HomePage(getDriver());
         homePage.open();
         Assert.assertTrue(homePage.isPageOpened(), "Home page was not opened!");
 
-        DropDownSearchForm dropDownSearchForm = homePage.getHeader().openDropDownSearchForm(SEARCH_TEXT);
+        DropDownSearchForm dropDownSearchForm = homePage.getHeader().openDropDownSearchForm(args.get("searchText"));
         Assert.assertTrue(dropDownSearchForm.isDropDownSearchFormIsVisible(), "Drop down search form was not opened!");
 
-        Assert.assertTrue(dropDownSearchForm.verifyListDevicesHaveText(SEARCH_TEXT), "Drop down search form didn't contains `" + SEARCH_TEXT + "`!");
+        Assert.assertTrue(dropDownSearchForm.verifyListDevicesHaveText(args.get("searchText")), "Drop down search form didn't contains `" + args.get("searchText") + "`!");
 
         DevicePage devicePage = dropDownSearchForm.openRandomDevicePage();
-        Assert.assertTrue(devicePage.isPageOpened(), SEARCH_TEXT + " page was not opened!");
-        Assert.assertTrue(devicePage.isPageHasSearchTextInHead(SEARCH_TEXT), SEARCH_TEXT + " page didn't have - `" + SEARCH_TEXT + "`!");
+        Assert.assertTrue(devicePage.isPageOpened(), args.get("searchText") + " page was not opened!");
+        Assert.assertTrue(devicePage.isPageHasSearchTextInHead(args.get("searchText")), args.get("searchText") + " page didn't have - `" + args.get("searchText") + "`!");
     }
 }
