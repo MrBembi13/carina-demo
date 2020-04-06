38c455bf27ff85b7bb6729e6b2ab420cce3c2f14
TA_1 <ta_1@Nastias-Mac-mini.local>
2020-04-06 18:24:23 +0300

    added bubble sort

---
+++ src/main/java/com/qaprosoft/carina/demo/utils/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.qaprosoft.carina.demo.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BubbleSort {
+    private static List<Integer> numbers = Arrays.asList(7, 8, 1, 4, 5);
+
+    public static void isListSorted() {
+        for (int j = 0; j < numbers.size(); j++) {
+            for (int i = 1; i < numbers.size() - j; i++) {
+                if (!(numbers.get(i-1) <= numbers.get(i))) {
+                    int x = numbers.get(i - 1);
+                    numbers.set(i - 1, numbers.get(i));
+                    numbers.set(i, x);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BubbleSort.isListSorted();
+    }
+}
